01154b54f979af1100e1fb7d870d3471e7fd851e
suroor <surusuroor@gmail.com>
2017-04-21 15:10:58 +0530

    Adds comments

--- fps.py
+++ fps.py
@@ -5,11 +5,11 @@ import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((640, 480))
-background = pygame.Surface(screen.get_size())
-background.fill((240, 240, 240))
-background = background.convert()
-screen.blit(background, (0, 0))
-pygame.display.flip()
+screen = pygame.display.set_mode((640, 480)) #set windo size
+background = pygame.Surface(screen.get_size()) #create a surface
+background.fill((240, 240, 240)) #fill a color
+background = background.convert() #for faster blitting
+screen.blit(background, (0, 0)) # attaxh background to screen
+pygame.display.flip() #update display
 
 time.sleep(2)
